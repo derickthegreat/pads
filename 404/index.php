1e6919a23dad22e2dec177ba02938b58dd31038d
federico villacillo <fbvillacillo@gmail.com>
2024-07-24 10:09:53 +0800

    error 404

---
+++ 404/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Access Denied</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="../css/w3school.css">
+
+<style>
+    body{
+        background-color: white;
+        color: black;
+    }
+
+    img{
+        width: 150px;
+        display: block;
+    }
+
+    h1, h6 {
+        color: darkslategray;
+        padding: 0px;
+        margin: 0px;
+    }
+
+    h6{
+        font-weight: lighter;
+    }
+
+    h6 a{
+        color: green;
+        font-weight: bolder;
+    }
+    .w3-display-middle{
+        width: 30%;
+    }
+    .img-container{
+        width: 30%;
+        float: left;
+    }
+    .txt-container{
+        width: 70%;
+        float: right;
+    }
+</style>
+</head>
+<body>
+<div class="w3-display-middle">
+    <div class="img-container">
+        <img src="../img/access-denied.jpg" alt="access-denied">    
+    </div>
+    <div class="txt-container">
+        <h1>Sorry!</h1>
+        <hr>
+        <h6>You are not authorized to access this page.</h6>
+        <h6>Click here to <a href="../" title="">Proceed</a> to homepage.</h6>
+    </div>
+    
+</div>
+</body>
+</html>
